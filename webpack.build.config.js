30f1d3c7107b8c3d49c6b7ab9a432673fb9c353e
dvdmgl <dvdmgl@gmail.com>
2019-08-04 19:46:58 +0100

    Clean duplicated webpack configuration
    added eslint:recommended
    extracted common webpack configuration to webpack.common.js

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -2,36 +2,10 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-// Any directories you will be adding code/files into, need to be added to this array so webpack will pick them up
-const defaultInclude = path.resolve(__dirname, 'src');
+const common = require('./webpack.common');
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
-        include: defaultInclude,
-      },
-      {
-        test: /\.jsx?$/,
-        use: [{ loader: 'babel-loader' }],
-        include: defaultInclude,
-      },
-      {
-        test: /\.(jpe?g|png|gif)$/,
-        use: [{ loader: 'file-loader?name=img/[name]__[hash:base64:5].[ext]' }],
-        include: defaultInclude,
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2)$/,
-        use: [{ loader: 'file-loader?name=font/[name]__[hash:base64:5].[ext]' }],
-        include: defaultInclude,
-      },
-    ],
-  },
-  target: 'electron-renderer',
+  ...common,
   plugins: [
     new HtmlWebpackPlugin(),
     new MiniCssExtractPlugin({
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,36 +2,10 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { spawn } = require('child_process');
-
-// Any directories you will be adding code/files into, need to be added to this array so webpack will pick them up
-const defaultInclude = path.resolve(__dirname, 'src');
+const common = require('./webpack.common');
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'postcss-loader' }],
-        include: defaultInclude,
-      },
-      {
-        test: /\.jsx?$/,
-        use: [{ loader: 'babel-loader' }],
-        include: defaultInclude,
-      },
-      {
-        test: /\.(jpe?g|png|gif)$/,
-        use: [{ loader: 'file-loader?name=img/[name]__[hash:base64:5].[ext]' }],
-        include: defaultInclude,
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2)$/,
-        use: [{ loader: 'file-loader?name=font/[name]__[hash:base64:5].[ext]' }],
-        include: defaultInclude,
-      },
-    ],
-  },
-  target: 'electron-renderer',
+  ...common,
   plugins: [
     new HtmlWebpackPlugin(),
     new webpack.DefinePlugin({
@@ -48,7 +22,7 @@ module.exports = {
     },
     before() {
       spawn('electron', ['.'], { shell: true, env: process.env, stdio: 'inherit' })
-        .on('close', code => process.exit(0))
+        .on('close', () => process.exit(0))
         .on('error', spawnError => console.error(spawnError));
     },
   },
